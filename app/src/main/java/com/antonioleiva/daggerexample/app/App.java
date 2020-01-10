743603dc74d9270b41387efb7d21a503ffb41ce4
Lundberg, Martin (Extern) <Martin.Lundberg.Extern@jsmd-group.com>
2020-01-10 13:41:01 +0100

    Remove unused import

--- app/src/main/java/com/antonioleiva/daggerexample/app/App.java
+++ app/src/main/java/com/antonioleiva/daggerexample/app/App.java
@@ -25,7 +25,6 @@ import android.app.Application;
 
 import com.antonioleiva.daggerexample.app.domain.AnalyticsManager;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
